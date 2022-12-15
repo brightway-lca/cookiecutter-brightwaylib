c3235db7b729c3cab58b86490f726b437398405a
Chris Mutel <cmutel@gmail.com>
2022-12-15 11:28:27 +0100

    Set `__version__` from file

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,1 +1,18 @@
-"""{{cookiecutter.package_name}}."""
+from pathlib import Path
+from typing import Union
+
+
+def get_version() -> tuple:
+    def as_integer(x: str) -> Union[int, str]:
+        try:
+            return int(x)
+        except ValueError:
+            return x
+
+    return tuple(
+        as_integer(v)
+        for v in open(Path(__file__).parent / "VERSION").read().strip().split(".")
+    )
+
+
+__version__ = get_version()
