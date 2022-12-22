879a6ed225f9fe93eb1fd2467d31eea61f9de556
Tomás NAVARRETE GUTIÉRREZ <tngTUDOR@users.noreply.github.com>
2022-12-22 12:38:22 +0100

    fix: add missing ref

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,8 +1,9 @@
 """{{cookiecutter.package_name}}."""
+from {{ cookiecutter.package_name }}.utils import get_version_tuple
+
 __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `{{cookiecutter.package_name}}.` namespace here
 )
-
 
 __version__ = get_version_tuple()
