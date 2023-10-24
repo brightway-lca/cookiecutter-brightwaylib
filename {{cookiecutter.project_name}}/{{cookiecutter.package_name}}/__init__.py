49526116eba3336e55e0b3a5633a6b2987d1b0f8
Chris Mutel <cmutel@gmail.com>
2023-10-24 12:36:42 +0200

    Much easier version specification

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,9 +1,8 @@
 """{{cookiecutter.package_name}}."""
-from {{ cookiecutter.package_name }}.utils import get_version_tuple
 
 __all__ = (
     "__version__",
     # Add functions and variables you want exposed in `{{cookiecutter.package_name}}.` namespace here
 )
 
-__version__ = get_version_tuple()
+__version__ = "0.0.1"
