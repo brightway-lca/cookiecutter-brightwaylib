40b3f751119dae91a23e5d7c02d8a9705efbf872
Francisco Júnior <fjunior.alves.oliveira@gmail.com>
2023-11-15 17:39:04 -0300

    add missing quotes to package version in  __init__.py

--- {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.project_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -5,4 +5,4 @@ __all__ = (
     # Add functions and variables you want exposed in `{{cookiecutter.package_name}}.` namespace here
 )
 
-__version__ = {{cookiecutter.version}}
+__version__ = "{{cookiecutter.version}}"
